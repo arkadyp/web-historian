2f716cc7a4b5037cca89268a4754964b99ccef03
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-01-29 14:53:34 -0800

    Refactors request-handler to make use of object with key-values for request type & func.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,8 +1,28 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
+var get = function(req, res){
+  archive.getIndexPage(function(data){
+    httpHelpers.sendResponse(res, data, 200);
+  });
+};
+
+var post = function(req, res){
+};
+
+var options = function(req, res){
+};
+
+var methods = {
+  'GET': get,
+  'POST': post,
+  'OPTIONS': options
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var method = methods[req.method];
+  method ? method(req, res) : httpHelpers.sendResponse(res, null, 404);
 };
 
