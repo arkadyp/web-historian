00e18c64824c49b4eb5abcc93311feff591c5305
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-01-29 17:35:55 -0800

    Reads URLs from file before server stars in basic-server

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,14 @@
 var http = require("http");
 var handler = require("./request-handler");
+var httpHelpers = require('./http-helpers');
+var archive = require('../helpers/archive-helpers');
 
 var port = 8080;
 var ip = "127.0.0.1";
+
+archive.readListOfUrls(); //This can cause problems if user begins to query server before list is read
 var server = http.createServer(handler.handleRequest);
+
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
