ed52f594f44c2e1ba36b11544458cca5093b29ab
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-01-29 16:23:20 -0800

    Updates http-helpers to allow for passed in content-type

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -6,8 +6,8 @@ exports.headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  "access-control-max-age": 10 // Seconds.
+  // 'Content-Type': "text/html"
 };
 
 exports.serveAssets = function(res, asset) {
@@ -15,7 +15,13 @@ exports.serveAssets = function(res, asset) {
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
 };
 
-exports.sendResponse = function(response, object, status){
+exports.sendResponse = function(response, object, status, contentType){
+  if(contentType) {
+    headers['Content-Type'] = contentType;
+  } else {
+    headers['Content-Type'] = "text/html";
+  }
+
   status = status || 200;
   response.writeHead(status, headers);
   response.end(object);
